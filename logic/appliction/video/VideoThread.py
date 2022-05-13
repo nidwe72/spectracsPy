ea0b86763afb889df3521e2226815a4428291a8c
nidwe72 <edwin.roth@gmx.at>
2022-05-13 21:49:18 +0200

    VideoThread: managed to run the french cam with highest resolution and manual exposure via opencv; managed to enumerate the cameras via QCamera letting distinct between different camers (opencv seems to have no enumeration at all)

--- logic/appliction/video/VideoThread.py
+++ logic/appliction/video/VideoThread.py
@@ -2,6 +2,8 @@ from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtCore import QThread
 from model.application.video.VideoSignal import VideoSignal
 from PyQt6.QtGui import QImage
+from PyQt6.QtMultimedia import QCamera
+from PyQt6.QtMultimedia import QMediaDevices
 import cv2
 
 class VideoThread(QThread):
@@ -15,16 +17,52 @@ class VideoThread(QThread):
 
     def run(self):
 
+        # camera = QCamera()
+        # videoInputs = QMediaDevices.videoInputs()
+        # for cameraDevice in videoInputs:
+        #     cameraName=cameraDevice.description()
+        #     print(cameraName)
+        #     cameraId=cameraDevice.id()
+        #     print("cameraDevice.position()")
+        #     print(cameraDevice.position())
+        #
+        #     print("cameraDevice.photoResolutions()")
+        #     print(cameraDevice.photoResolutions())
+        #
+        #     print(cameraId)
+        #     cameraDeviceFormats=cameraDevice.videoFormats()
+        #     print(cameraDeviceFormats)
+        #     for cameraDeviceFormat in cameraDeviceFormats:
+        #         print(cameraDeviceFormat.resolution())
+        #         print(cameraDeviceFormat.pixelFormat())
+
+        #self.cap = cv2.VideoCapture(4)
         self.cap = cv2.VideoCapture(0)
+
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        self.cap.set(cv2.CAP_PROP_FOURCC, fourcc)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+        # bar=cv2.VideoWriter.get(cv2.VIDEOWRITER_PROP_HW_DEVICE)
+        # print(bar)
+
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE,1)
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, 150)
+        #self.cap.set(cv2.CAP_PROP_EXPOSURE, 300)
+        # foo=""
+        # foo=self.cap.get(cv2.CAP_PROP_EXPOSURE)
+        # print(foo)
+        # print(fourcc)
+
         while self._run_flag:
             ret, frame = self.cap.read()
             if ret:
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 h, w, ch = rgbImage.shape
                 bytesPerLine = ch * w
-                # qImage = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format.Format_RGB32)
+                #needs QImage.Format.Format_RGB888 or crashes for some reason after some frames
                 qImage = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format.Format_RGB888)
-
 
                 videoSignal= VideoSignal()
                 videoSignal.image=qImage
