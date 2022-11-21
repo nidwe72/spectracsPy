d501627b02be5335905df64a6f6af54ba73fc651
nidwe72 <edwin.roth@gmx.at>
2022-11-21 21:10:40 +0100

    * VirtualCameraViewModule: initial commit

--- view/settings/spectral/spectrometer/acquisition/device/virtualCamera/VirtualCameraViewModule.py
+++ view/settings/spectral/spectrometer/acquisition/device/virtualCamera/VirtualCameraViewModule.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QCheckBox, QPushButton
+from PySide6.QtWidgets import QCheckBox, QPushButton, QFileDialog
 
 from logic.appliction.style.ApplicationStyleLogicModule import ApplicationStyleLogicModule
 from view.application.widgets.general.ToggleSwitch import ToggleSwitch
@@ -8,6 +8,7 @@ from view.application.widgets.page.PageWidget import PageWidget
 class VirtualCameraViewModule(PageWidget):
 
     useVirtualCameraComponent:ToggleSwitch=None
+    openPictureButton:QPushButton=None
 
     def createMainContainer(self):
         result=super().createMainContainer()
@@ -18,13 +19,21 @@ class VirtualCameraViewModule(PageWidget):
         result = super().getMainContainerWidgets()
 
         self.useVirtualCameraComponent=ToggleSwitch(None,Qt.gray,ApplicationStyleLogicModule().getPrimaryColor())
-        self.useVirtualCameraComponent.setCheckable(True)
-        # self.useVirtualCameraCheckbox.setReadOnly(True)
+        #self.useVirtualCameraComponent.setCheckable(True)
         result['useVirtualCameraComponent']=self.createLabeledComponent('use virtual camera',self.useVirtualCameraComponent)
+
+        self.openPictureButton=QPushButton('Set picture')
+        result['openPictureButton'] = self.openPictureButton
+        self.openPictureButton.clicked.connect(self.onClickedOpenPictureButton)
 
         return result
 
     def _getPageTitle(self):
         return "Virtual camera"
+
+
+    def onClickedOpenPictureButton(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open picture',
+                                            None, "Image files (*.jpg *.gif)")
 
 
