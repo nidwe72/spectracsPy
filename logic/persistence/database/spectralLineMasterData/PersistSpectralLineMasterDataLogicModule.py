d9d895ea3158cd390267082565620b6d9bbf1302
nidwe72 <edwin.roth@gmx.at>
2023-03-27 19:21:23 +0200

    * topic: let the user select the SpectrometerProfile * SpectrometerConnectionViewModule: continued * now we can provide a persistence logic module, which uses a template entity having some dirty members used to build up the select statement in a generic fashion (using the SqlUtil class)

--- logic/persistence/database/spectralLineMasterData/PersistSpectralLineMasterDataLogicModule.py
+++ logic/persistence/database/spectralLineMasterData/PersistSpectralLineMasterDataLogicModule.py
@@ -1,5 +1,9 @@
 from typing import Dict
 
+from sqlalchemy.orm import class_mapper
+from sqlalchemy import inspect
+
+from logic.model.util.databaseEntity.SqlUtil import SqlUtil
 from logic.persistence.database.spectralLineMasterData.PersistenceParametersGetSpectralLineMasterDatas import \
     PersistenceParametersGetSpectralLineMasterDatas
 from model.databaseEntity.DbBase import session_factory
@@ -13,8 +17,8 @@ class PersistSpectralLineMasterDataLogicModule:
         session.add(spectralLineMasterData)
         session.commit()
 
-    def getSpectralLineMasterDatas(self,
-                                   persistenceParametersGetSpectralLineMasterDatas: PersistenceParametersGetSpectralLineMasterDatas) -> \
+    def getSpectralLineMasterDatasOld(self,
+                                      persistenceParametersGetSpectralLineMasterDatas: PersistenceParametersGetSpectralLineMasterDatas) -> \
             Dict[int, SpectralLineMasterData]:
         ids = persistenceParametersGetSpectralLineMasterDatas.getIds()
         session = session_factory()
@@ -24,4 +28,27 @@ class PersistSpectralLineMasterDataLogicModule:
         for spectralLineMasterData in resultList:
             result[spectralLineMasterData.id] = spectralLineMasterData
 
+        return result
+
+    def getSpectralLineMasterDatas(self,
+                                   moduleParameters: PersistenceParametersGetSpectralLineMasterDatas) -> \
+            Dict[int, SpectralLineMasterData]:
+
+        if moduleParameters is None:
+            moduleParameters = PersistenceParametersGetSpectralLineMasterDatas()
+
+        baseEntity = moduleParameters.getBaseEntity()
+        selectStatement = SqlUtil.createSelect(baseEntity)
+        result = SqlUtil.executeSelect(baseEntity, selectStatement)
+
+        return result
+
+    def getChangedAttributes(self, entity):
+        result = []
+        inspr = inspect(entity)
+        attrs = class_mapper(entity.__class__).column_attrs  # exclude relationships
+        for attr in attrs:
+            hist = getattr(inspr.attrs, attr.key).history
+            if hist.has_changes():
+                result.append(attr)
         return result
--- logic/persistence/database/spectralLineMasterData/PersistenceParametersGetSpectralLineMasterDatas.py
+++ logic/persistence/database/spectralLineMasterData/PersistenceParametersGetSpectralLineMasterDatas.py
@@ -1,7 +1,23 @@
 from typing import List
 
+from model.databaseEntity.spectral.device.SpectralLineMasterData import SpectralLineMasterData
+
+
 class PersistenceParametersGetSpectralLineMasterDatas:
-    ids: List = []
+
+    def __init__(self, *args, **kwargs):
+        self.ids: List = []
+        self.__baseEntity= None
+
+    def getBaseEntity(self):
+        if self.__baseEntity is None:
+            self.__baseEntity=SpectralLineMasterData()
+        return self.__baseEntity
+
+    def setBaseEntity(self, baseEntity):
+        self.__baseEntity=baseEntity
+        return self
+
 
     def setIds(self, ids: List):
         self.ids = ids
