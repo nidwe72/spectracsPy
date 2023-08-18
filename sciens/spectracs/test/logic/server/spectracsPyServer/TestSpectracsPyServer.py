f1f9225d07d003b5e3c1b6b9a26b6d7e0e842e11
nidwe72 <edwin.roth@gmx.at>
2023-08-18 12:56:19 +0200

    * [SpectracsPyServer] * now using Pyro5 * serializing and persisting now seems to work

--- sciens/spectracs/test/logic/server/spectracsPyServer/TestSpectracsPyServer.py
+++ sciens/spectracs/test/logic/server/spectracsPyServer/TestSpectracsPyServer.py
@@ -1,12 +1,16 @@
 
+import serpent
+import Pyro5.api
+import Pyro5.client
+# from Pyro5.utils import getPyroTraceback
 
-import Pyro4
+from sciens.spectracs.logic.model.util.SpectrometerUtil import SpectrometerUtil
 
 
 class TestSpectracsPyServer:
 
     # def test_test(self):
-    #     spectracsPyServer = Pyro4.Proxy('PYRO:sciens.SpectracsPyServer@localhost:41145')
+    #     spectracsPyServer = Pyro5.Proxy('PYRO:sciens.SpectracsPyServer@localhost:41145')
     #     version = spectracsPyServer.getVersion()
     #     print(version)
     #     assert version is not None
@@ -16,15 +20,34 @@ class TestSpectracsPyServer:
         # .8
         # .111: 8090
 
+        # Pyro5.config.SERIALIZER = "json"
+
         host = "192.168.8.111"
         port = 8090
-        nameserver = Pyro4.locateNS(host=host,port=port)
+        nameserver = Pyro5.api.locate_ns(host=host,port=port)
         uri = nameserver.lookup("sciens.spectracs.spectracsPyServer")
 
-        spectracsPyServer = Pyro4.Proxy(uri)
-        persistentSpectrometers = spectracsPyServer.getPersistentSpectrometers()
-        print('=====persistentSpectrometers=====')
-        print(persistentSpectrometers)
+        spectracsPyServer = Pyro5.client.Proxy(uri)
+        # persistentSpectrometers = spectracsPyServer.getPersistentSpectrometers()
+
+        localSpectrometers = SpectrometerUtil().getSpectrometers()
+
+        # to_dict = next(iter(localSpectrometers.values())).to_dict()
+
+        try:
+            # serialize = serpent.dumps(localSpectrometers)
+            # tobytes = serpent.tobytes(localSpectrometers)
+
+
+
+            spectracsPyServer.syncSpectrometers(localSpectrometers)
+        except Exception:
+            print('')
+            # traceback = getPyroTraceback()
+            # print(traceback)
+
+        # print('=====persistentSpectrometers=====')
+        # print(persistentSpectrometers)
         assert 1==1
 
 
